b87395f8e26be8ac5da6da9dc1ebb5d904159832
SWH2025 <shaowenhao0058@gmail.com>
2026-09-08 11:25:35 +0800

    实现角色创建后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 from web.model.user import UserProfile
+from web.model.character import Character
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",)
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
