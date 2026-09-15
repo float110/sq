34e27f6e5c30eb000ebf821de7fb5eab137cd0e8
swh <211440776@qq.com>
2026-09-15 11:33:53 +0800

    实现角色卡片

--- frontend/src/views/user/space/SpaceIndex.vue
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -3,6 +3,7 @@ import {useRoute} from "vue-router";
 import UserInfoField from "@/views/user/space/components/UserInfoField.vue";
 import {nextTick, onBeforeUnmount, onMounted, ref} from "vue";
 import api from "@/js/http/api.js";
+import Character from "@/components/character/Character.vue";
 
 
 const route = useRoute()
@@ -71,12 +72,22 @@ onBeforeUnmount(() => {
   observer?.disconnect()  // 解绑监听器
 })
 
+function RemoveCharacter(characterId){
+  characters.value = characters.value.filter(c => c.id !== characterId)
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center mb-12">
     <UserInfoField :userProfile="userProfile" />
     <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+      <Character
+          v-for="character in characters"
+          :key="character.id"
+          :character="character"
+          :canEdit="true"
+          @remove="RemoveCharacter"
+      />
     </div>
     <div ref="sentinel-ref" class="h-2 mt-8 w-100 bg-red-500"></div>
     <div v-if="isLoading" class="text-gray-500 mt-4">加载中...</div>
